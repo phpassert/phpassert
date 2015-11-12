b8e5f36bf1ea5b44c1fcf5201286932e21cec17d
Thomas Farla <thomas-farla@hotmail.com>
2015-11-12 13:25:01 +0100

    Setup testing

---
+++ bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+
+ini_set('assert.exception', 1);
+require_once('vendor/autoload.php');
---
+++ tests/test.php
@@ -0,0 +1,6 @@
+<?php
+
+function testTest()
+{
+    assert(true, new \AssertionError('Testing is not properly set up'));
+}
